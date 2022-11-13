3ebc37db916303e3ef477c40b03223fc7f6767cb
aigerimass <assylkhanova0@gmail.com>
2022-11-13 22:07:10 +0300

    BinomialHeap plus doesn't work

--- homework02/lib/src/main/kotlin/binomial/BinomialHeap.kt
+++ homework02/lib/src/main/kotlin/binomial/BinomialHeap.kt
@@ -25,7 +25,7 @@ package binomial
  */
 class BinomialHeap<T: Comparable<T>> private constructor(private val trees: FList<BinomialTree<T>?>): SelfMergeable<BinomialHeap<T>> {
     companion object {
-        fun <T: Comparable<T>> single(value: T): BinomialHeap<T> = TODO()
+        fun <T: Comparable<T>> single(value: T): BinomialHeap<T> = BinomialHeap(flistOf(BinomialTree.single(value)))
     }
 
     /*
@@ -33,23 +33,35 @@ class BinomialHeap<T: Comparable<T>> private constructor(private val trees: FLis
      *
      * Требуемая сложность - O(log(n))
      */
-    override fun plus(other :BinomialHeap<T>): BinomialHeap<T> = TODO()
+    override fun plus(other :BinomialHeap<T>): BinomialHeap<T> {
+        if (trees.isEmpty) return other
+        if (other.trees.isEmpty) return other
+        val treesCons = trees as FList.Cons
+        val oTreesCons = other.trees as FList.Cons
+        if (treesCons.head!!.order == oTreesCons.head!!.order)
+            return BinomialHeap(FList.Cons(oTreesCons.head.plus(treesCons.head),
+                BinomialHeap(oTreesCons.tail).plus(BinomialHeap(treesCons.tail)).trees))
+        if (treesCons.head.order < oTreesCons.head.order) {
+            return BinomialHeap(FList.Cons(treesCons.head, FList.Cons(oTreesCons.head,
+                BinomialHeap(oTreesCons.tail).plus(BinomialHeap(treesCons.tail)).trees)))
+        }
+        return BinomialHeap(FList.Cons(oTreesCons.head, FList.Cons(treesCons.head,
+                BinomialHeap(oTreesCons.tail).plus(BinomialHeap(treesCons.tail)).trees)))
+    }
 
     /*
      * добавление элемента
      * 
      * Требуемая сложность - O(log(n))
      */
-    operator fun plus(elem: T): BinomialHeap<T> = TODO()
+    operator fun plus(elem: T): BinomialHeap<T> = plus(single(elem))
 
     /*
      * минимальный элемент
      *
      * Требуемая сложность - O(log(n))
      */
-    fun top(): T {
-        TODO()
-    }
+    fun top(): T = if (trees.isEmpty) throw NoSuchElementException() else trees.minOf { tree -> tree!!.value }
 
     /*
      * удаление элемента
