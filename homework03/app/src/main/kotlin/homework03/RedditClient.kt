a07363bce1874253685902ec08c9574093246fea
aigerimass <assylkhanova0@gmail.com>
2022-12-11 20:24:34 +0300

    parsed posts

--- homework03/app/src/main/kotlin/homework03/RedditClient.kt
+++ homework03/app/src/main/kotlin/homework03/RedditClient.kt
@@ -2,7 +2,8 @@ package homework03
 
 import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.ObjectMapper
-import homework03.dto.JsonMetaInfoWrapper
+import homework03.dto.JsonAboutWrapper
+import homework03.dto.JsonPostsWrapper
 import homework03.dto.TopicSnapshot
 import io.ktor.client.*
 import io.ktor.client.call.*
@@ -18,8 +19,16 @@ object RedditClient {
     }
 
     suspend fun getTopic(name: String): TopicSnapshot {
-        val metaInfoJson = httpClient.get("https://www.reddit.com/r/$name/about.json").body<String>()
-        val topicMainData = objectMapper.readValue(metaInfoJson, JsonMetaInfoWrapper::class.java)
-        return TopicSnapshot.get(topicMainData)
+        return TopicSnapshot.get(getAboutTopic(name), getPosts(name))
+    }
+
+    private suspend fun getAboutTopic(name: String): JsonAboutWrapper.TopicAbout {
+        val aboutTopicJson = httpClient.get("https://www.reddit.com/r/$name/about.json").body<String>()
+        return objectMapper.readValue(aboutTopicJson, JsonAboutWrapper::class.java).data
+    }
+
+    private suspend fun getPosts(name: String): JsonPostsWrapper.JsonPosts {
+        val postsJson = httpClient.get("https://www.reddit.com/r/$name/.json").body<String>()
+        return objectMapper.readValue(postsJson, JsonPostsWrapper::class.java).data
     }
 }
--- homework03/app/src/main/kotlin/homework03/dto/Topic.kt
+++ homework03/app/src/main/kotlin/homework03/dto/Topic.kt
@@ -3,7 +3,29 @@ package homework03.dto
 import com.fasterxml.jackson.annotation.JsonProperty
 import java.util.*
 
-data class JsonMetaInfoWrapper(@JsonProperty("data") val topicAbout: TopicAbout) {
+var countSnapshot = 0
+data class TopicSnapshot(
+    val publishDate: Date,
+    val activeSubscribers: Long,
+    val sizeRanking: String?,
+    val description: String,
+    val posts: List<Post>
+) {
+    val id = ++countSnapshot
+    val downloadTime = System.currentTimeMillis()
+
+    companion object {
+        fun get(topicAbout: JsonAboutWrapper.TopicAbout, JsonPosts: JsonPostsWrapper.JsonPosts) = TopicSnapshot(
+            publishDate = Date(topicAbout.createdEpochTime.toLong() * 1000),
+            activeSubscribers = topicAbout.activeUserCount,
+            sizeRanking = topicAbout.rankingSize,
+            description = topicAbout.publicDescription,
+            posts = JsonPosts.jsonPostWrappers.map {it.data}
+        )
+    }
+}
+
+data class JsonAboutWrapper(@JsonProperty("data") val data: TopicAbout) {
     data class TopicAbout(
         @JsonProperty("created") val createdEpochTime: Double,
         @JsonProperty("active_user_count") val activeUserCount: Long,
@@ -12,18 +34,19 @@ data class JsonMetaInfoWrapper(@JsonProperty("data") val topicAbout: TopicAbout)
     )
 }
 
-data class TopicSnapshot(
-    val publishDate: Date,
-    val activeSubscribers: Long,
-    val sizeRanking: String?,
-    val description: String
-) {
-    companion object {
-        fun get(metaInfo: JsonMetaInfoWrapper) = TopicSnapshot(
-            publishDate = Date(metaInfo.topicAbout.createdEpochTime.toLong() * 1000),
-            activeSubscribers = metaInfo.topicAbout.activeUserCount,
-            sizeRanking = metaInfo.topicAbout.rankingSize,
-            description = metaInfo.topicAbout.publicDescription
-        )
+data class Post(
+    @JsonProperty("author_fullname") val author: String,
+    @JsonProperty("created") val createdEpochTime: Double,
+    @JsonProperty("ups") val upVotes: Long,
+    @JsonProperty("downs") val downVotes: Long,
+    @JsonProperty("title") val title: String,
+    @JsonProperty("selftext") val selftext: String?,
+    @JsonProperty("selftext_html") val selfhtmlText: String?
+)
+
+data class JsonPostsWrapper(@JsonProperty("data") val data: JsonPosts) {
+    data class JsonPosts(@JsonProperty("children") val jsonPostWrappers: List<JsonPostWrapper>) {
+        data class JsonPostWrapper(@JsonProperty("data") val data: Post)
     }
 }
+
