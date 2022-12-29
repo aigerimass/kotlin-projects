600c1dfe74eed531070555e44ace7facd12b1819
aigerimass <assylkhanova0@gmail.com>
2022-12-29 14:16:06 +0300

    minor fix

--- homework03/app/src/main/kotlin/homework03/RedditClient.kt
+++ homework03/app/src/main/kotlin/homework03/RedditClient.kt
@@ -14,10 +14,10 @@ import io.ktor.client.request.*
 object RedditClient {
     private val HTTP_CLIENT = HttpClient(CIO)
     private val OBJECT_MAPPER = ObjectMapper()
-    private val MAIN_URL = "https://www.reddit.com/r/"
-    private val COMM_URL = "/comments/"
-    private val JSON_URL = "/.json"
-    private val ABOUT_URL = "/about.json"
+    private val MAIN_URL = "https://www.reddit.com/r"
+    private val COMM_URL = "comments"
+    private val JSON_URL = ".json"
+    private val ABOUT_URL = "about.json"
 
     init {
         OBJECT_MAPPER.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
@@ -28,21 +28,25 @@ object RedditClient {
     }
 
     private suspend fun getAboutTopic(name: String): JsonAboutWrapper.TopicAbout {
-        val json = HTTP_CLIENT.get(MAIN_URL + name + ABOUT_URL).body<String>()
-        return OBJECT_MAPPER.readValue(json, JsonAboutWrapper::class.java).data
+        return OBJECT_MAPPER.readValue(getTopicJsonFiles(name, ABOUT_URL),
+            JsonAboutWrapper::class.java).data
     }
 
     private suspend fun getPosts(name: String): JsonPostsWrapper.JsonPosts {
-        val json = HTTP_CLIENT.get(MAIN_URL + name + JSON_URL).body<String>()
-        return OBJECT_MAPPER.readValue(json, JsonPostsWrapper::class.java).data
+        return OBJECT_MAPPER.readValue(getTopicJsonFiles(name, JSON_URL),
+            JsonPostsWrapper::class.java).data
     }
 
     suspend fun getComments(url: String): CommentsSnapshot {
-        val json = HTTP_CLIENT.get(url + JSON_URL).body<String>()
+        val json = HTTP_CLIENT.get("$url/$JSON_URL").body<String>()
         return CommentsSnapshot.parse(OBJECT_MAPPER, json)
     }
 
     suspend fun getComments(topicName: String, threadID: String): CommentsSnapshot {
-        return getComments(MAIN_URL + topicName + COMM_URL + threadID)
+        return getComments("$MAIN_URL/$topicName/$COMM_URL/$threadID")
+    }
+
+    private suspend fun getTopicJsonFiles(topicName: String, jsonFile: String): String {
+        return HTTP_CLIENT.get("$MAIN_URL/$topicName/$jsonFile").body()
     }
 }
